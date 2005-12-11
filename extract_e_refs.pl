a38e8908dfd2e1884515ab15d612210ad3d64205
urban <urban>
2005-12-11 18:52:07 +0000

    usage added

--- extract_e_refs.pl
+++ extract_e_refs.pl
@@ -1,5 +1,17 @@
 #!/usr/bin/perl -w
 
+# SYNOPSIS:
+
+# Run E on provable problems and save proof premises into *.proof:
+
+# for i in `cat 00proved`;do echo $i;
+# eprover -l4 --pcl-compact --tstp-in -xAuto -tAuto $i | \
+# epclextract  --tstp-out | grep file > $i.proof; done
+
+# Then extract the references into 00proofs:
+
+# for i in `ls *.proof`; do extract_e_refs.pl $i; done > 00proofs
+
 # extract the references from E proof in tstp syntax;
 # put them into 
 # snow_proof( conjecture_name, th_or_def_names, bg_names)
