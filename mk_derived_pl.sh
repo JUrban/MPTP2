c72b696a45ebd9433c6c8195e62a2e4063210a3e
mizarcvs <mizarcvs>
2006-07-05 17:06:41 +0000

    added lem2 for top-level lemmas - needed for theorem dependence analysis

--- mk_derived_pl.sh
+++ mk_derived_pl.sh
@@ -3,4 +3,5 @@ for i in tarski `cat mml.lar`; do
     grep "fof([dt][0-9]" pl/$i.xml2 > pl/$i.the2; 
     grep "fof([fcr]c[0-9]" pl/$i.xml2 > pl/$i.dcl2;
     grep "fof([s][0-9]" pl/$i.xml2 > pl/$i.sch2;
+    grep "fof([l][0-9]" pl/$i.xml2 > pl/$i.lem2;
 done
