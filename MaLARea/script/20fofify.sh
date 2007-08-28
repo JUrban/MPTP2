be844c100ee0ecbee7546eb8b9e635cd34d2b6ef
mizarcvs <mizarcvs>
2007-08-28 23:18:16 +0000

    added the MAL_DIR for customization, info printing

--- MaLARea/script/20fofify.sh
+++ MaLARea/script/20fofify.sh
@@ -1,19 +1,39 @@
+export MAL_DIR=/home/urban/mptp/MaLARea
+echo "downcasing"
+echo "##############################"
+for i in `ls`; do j=`echo $i | tr [A-Z] [a-z]`; echo $i; mv $i $j; done
 mkdir axioms conjectures0 conjectures fofified renamed0 renamed
+echo "fofifying axioms"
+echo "##############################"
 for i in `ls *.p`; do echo $i; grep "cnf([^,]*,axiom," $i | tptp4X -t fofify -u machine -- >axioms/$i; done
 for i in `ls *.p`; do echo $i; grep -v "cnf([^,]*,axiom," $i >conjectures0/$i; done
 cd conjectures0
 mkdir res 
+echo "fofifying conjectures"
+echo "##############################"
 for i in `ls`; do /home/urban/TPTPWorld/TPTP/TPTP2X/tptp2X  -d `pwd`/res  -t fofify:obvious  $i; done
 cd res
 for i in `ls`; do j=`echo -n $i | sed -e 's/.fof_obvious.tptp//'`; tptp4X -u machine -c $i | sed -e "s/^fof([^,]*,/fof($j,/" > ../../conjectures/$j.p; done
 cd ../..
-for i in `ls *.p`; do echo $i; cat axioms/$i conjectures/$i | ../../NumberVars > fofified/$i; done
+echo "merging axioms and conjectures"
+echo "##############################"
+for i in `ls *.p`; do echo $i; cat axioms/$i conjectures/$i | $MAL_DIR/bin/NumberVars > fofified/$i; done
 cd fofified
+echo "sorting formulas into 00"
+echo "##############################"
 cat *| sort -u >00
-../../../rename_from_sorted.pl
+echo "merging same formulas"
+echo "##############################"
+$MAL_DIR/script/rename_from_sorted.pl
 cd ../renamed0
 sort -u 00 >01
-../../../mk_diff_names.pl
+echo "renaming different formulas with the same name"
+echo "##############################"
+$MAL_DIR/script/mk_diff_names.pl
 cd ../renamed
-# for i in `ls *.p`; do j=`echo -n $i| sed -e 's/.p$//'`; echo $j; sed -e "s/^fof([^,]*,conjecture/fof($j,conjecture/" $i >$i.new; done
-for i in `ls *.p`; do j=`echo -n $i| sed -e 's/.p$//'`; grep -L "^fof($j,conjecture" $i; done
+echo "finding problems with not their own conjecture (see file 02)"
+echo "##############################"
+for i in `ls *.p`; do j=`echo -n $i| sed -e 's/.p$//'`; grep -L "^fof($j,conjecture" $i; done |tee 02
+echo "renaming conjectures"
+echo "##############################"
+for i in `ls *.p`; do j=`echo -n $i| sed -e 's/.p$//'`; echo $j; sed -e "s/^fof([^,]*,conjecture/fof($j,conjecture/" $i >$i.new; done
