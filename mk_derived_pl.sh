d7247a2b8b22ebb070cd6d6ef179cbd0de2f6afe
mizarcvs <mizarcvs>
2007-09-18 22:57:20 +0000

    LANG=C

--- mk_derived_pl.sh
+++ mk_derived_pl.sh
@@ -1,5 +1,7 @@
 # make the dcl2,sch2 and the2 files from xml2
+LANG=C;
 for i in tarski `cat mml.lar`; do 
+    echo $i;
     grep "fof([dt][0-9]" pl/$i.xml2 > pl/$i.the2; 
     grep "fof([fcr]c[0-9]" pl/$i.xml2 > pl/$i.dcl2;
     grep "fof([s][0-9]" pl/$i.xml2 > pl/$i.sch2;
