d053ee7b0f2df7d105023cd769b616af4be47e09
mizarcvs <mizarcvs>
2008-01-31 15:47:37 +0000

    fixes for initial passes

--- MaLARea/script/mkloggraph.pl
+++ MaLARea/script/mkloggraph.pl
@@ -3,6 +3,11 @@
 # SYNOPSIS
 # ./mkloggraph.pl unsolved.log >unsolved.stats
 # creates summary of results of all passes in unsolved.log
+
+
+$solved = 0;
+$total = 0;
+$state = 0;
 $giter = 0;
 $theoremnbr = 0;
 $timelimit[0] = 1;
@@ -13,6 +18,7 @@ while (<>)
       $solved[$giter] = 1+$1; 
       die "Theorems not in sync: $1: $theoremnbr" if(($giter> 3) && (1+$1 != $theoremnbr));
     }
+  if(m/SYMBOL ONLY/) { $solved[$giter] = $theoremnbr; $state = 1; } 
   if(m/THRESHOLD: *([0-9]+)/) 
     { 
        $giter++; $threshold[$giter] = $1; $theoremnbr = 0;
@@ -20,13 +26,29 @@ while (<>)
       $solved[$giter] = 0;
      }
   if(m/TIMELIMIT: *([0-9]+)/) { $timelimit[$giter] = $1; }
-  if(m/LEARNING: *([0-9]+)/) { $learning[$giter] = $1; }
+  if(m/LEARNING: *([0-9]+)/) 
+    { 
+      $learning[$giter] = $1; 
+      if($state == 1) 
+	{
+	  $solved[$giter] = $theoremnbr;
+	  $giter++; $threshold[$giter] = $threshold[$giter - 1]; $theoremnbr = 0;
+	  $timelimit[$giter] = $timelimit[$giter - 1];
+	  $solved[$giter] = 0;
+	  $state = 2;
+	}
+    }
   if(m/Theorem/) { $theoremnbr++; }
+  if(($state == 0) && (m/.*:.*:/)) { $total++; }
 }
 #print "$giter\n";
 
+foreach $i (0 .. $giter) { $solved = $solved + $solved[$i]; }
+my $rest = $solved - $solved[0];
+
+print "total: $total, solved: $solved, pass 0: $solved[0], pass 1- : $rest\n";
 print "iternr\ttimelim\taxlimit\tsolved\n";
-foreach $i (1 .. $giter)
+foreach $i (0 .. $giter)
   {
     print "$i:\t$timelimit[$i]\t$threshold[$i]\t$solved[$i]\n";
   }
