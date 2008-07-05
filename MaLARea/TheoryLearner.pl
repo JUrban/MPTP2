d444f060d5511db90c5ad9822f88a57660c4c095
mizarcvs <mizarcvs>
2008-07-05 22:06:37 +0000

    problems with gatpdata again

--- MaLARea/TheoryLearner.pl
+++ MaLARea/TheoryLearner.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-## $Revision: 1.108 $
+## $Revision: 1.109 $
 
 
 =head1 NAME
@@ -622,6 +622,9 @@ sub opt_MAXCPU		()  { 3 }
 sub InitAtpData
 {
     %gatpdata = ();
+    @gatpdata{('atp_E','atp_EP','atp_SPASS','atp_VAMPIRE','atp_PARADOX',
+	       'atp_MACE','atp_RANDOCOP','atp_PROVER9')}
+	= ();
     foreach my $atp (@gallatps) { $gatpdata{ $atp } = [1000000000,-1,-1,-1]; }
 
     if ($gspass > 1) { $gatpdata{ 'atp_SPASS' }->[ opt_MAXREFS ] = $gspass; $gspass = 1; }
