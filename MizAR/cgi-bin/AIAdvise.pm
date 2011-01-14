918c5596363b71a6be956d28140fa2922a776fb9
Josef Urban <urban@eee.(none)>
2011-01-14 22:56:44 +0100

    added CreateTables

--- MizAR/cgi-bin/AIAdvise.pm
+++ MizAR/cgi-bin/AIAdvise.pm
@@ -5,11 +5,84 @@ use IO::Socket;
 
 sub min { my ($x,$y) = @_; ($x <= $y)? $x : $y }
 
-##  StartSNoW($path2snow, $path2advisor, $snow_symoffset, $snow_filestem);
+
+# CreateTables($symoffset, $filestem)
+#
+# Create the symbol and reference numbering files
+# from the refsyms file. Loads these tables and the refsym table tooa dn return pointers to them.
+# The initial refsyms file can be created from all (say bushy) problems by running:
+# cat */* | bin/GetSymbols -- |sort -u > all.refsyms
+sub CreateTables
+{
+    my ($symoffset, $filestem) = @_;
+    my $i = 0;
+    my ($ref,$sym,$trms,@syms,$psyms,$fsyms);
+
+    open(REFSYMS, "$filestem.refsyms") or die "Cannot read refsyms file";
+    open(REFNR, ">$filestem.refnr") or die "Cannot write refnr file";
+    open(SYMNR, ">$filestem.symnr") or die "Cannot write symnr file";
+
+    my %grefnr = ();	# Ref2Nr hash for references
+    my %gsymnr = ();	# Sym2Nr hash for symbols
+    my %gsymarity = ();	# for each symbol its arity and 'p' or 'f'
+    my %grefsyms = ();	# Ref2Sym hash for each reference array of its symbols
+    my @gnrsym = ();	# Nr2Sym array for symbols - takes symoffset into account!
+    my @gnrref = ();	# Nr2Ref array for references
+
+    while($_=<REFSYMS>)
+    {
+	chop; 
+	m/^symbols\( *([a-z0-9A-Z_]+) *, *\[(.*)\] *, *\[(.*)\] *\)\./ 
+	    or die "Bad symbols info: $_";
+	($ref, $psyms, $fsyms) = ($1, $2, $3);
+	my @psyms = split(/\,/, $psyms);
+	my @fsyms = split(/\,/, $fsyms);
+	die "Duplicate reference $ref in $_" if exists $grefnr{$ref};
+	$grefsyms{$ref} = [];
+	push(@gnrref, $ref);
+	$grefnr{$ref} = $#gnrref;
+	print REFNR "$ref\n";
+
+	## this now also remembers arity and symbol kind in %gsymarity
+	foreach $sym (@psyms)
+	{
+	    $sym =~ m/^ *([^\/]+)[\/]([^\/]+)[\/].*/ or die "Bad symbol $sym in $_";
+	    $gsymarity{$1} = [$2, 'p'];
+	    push(@{$grefsyms{$ref}}, $1);
+	}
+	foreach $sym (@fsyms)
+	{
+	    $sym =~ m/^ *([^\/]+)[\/]([^\/]+)[\/].*/ or die "Bad symbol $sym in $_";
+	    $gsymarity{$1} = [$2, 'f'];
+	    push(@{$grefsyms{$ref}}, $1);
+	}
+
+    }
+    close REFNR;
+    foreach $sym (keys %gsymarity)
+    {
+	print SYMNR "$sym\n";
+	push(@gnrsym, $sym);
+	$gsymnr{$sym} = $symoffset + $i++;
+    }
+    close SYMNR;
+    close REFSYMS;
+
+#    LoadTermTable("$filestem.trmstd",\%greftrmstd,$gstdtrmoffset) if($gdotrmstd > 0);
+#    LoadTermTable("$filestem.trmnrm",\%greftrmnrm,$gnrmtrmoffset) if($gdotrmnrm > 0);
+
+    my $gtargetsnr = $#gnrref;
+#    print $gtargetsnr . "\n";
+    return (\%grefnr, \%gsymnr, \%gsymarity, \%grefsyms, \@gnrsym, \@gnrref);
+
+}
+
+
+##  StartSNoW($path2snow, $path2advisor, $symoffset, $snow_filestem);
 ##
 ## Get unused ports for SNoW and for the symbol translation daemon
 ## (advisor), start them, and return the ports and the pids of snow
-## and advisor.  $snow_symoffset tells the translation daemon where
+## and advisor.  $symoffset tells the translation daemon where
 ## the symbol numbering starts.
 ##
 ## Be sure to sleep for sufficient amount of time (ca 40s for all MML)
@@ -22,7 +95,7 @@ sub min { my ($x,$y) = @_; ($x <= $y)? $x : $y }
 ## my ($aport, $sport, $adv_pid, $snow_pid) = StartSNoW("$BinDir/snow", "$BinDir/advisor.pl", 500000, 'test1');
 sub StartSNoW
 {
-    my ($path2snow, $path2advisor, $snow_symoffset, $snow_filestem) = @_;
+    my ($path2snow, $path2advisor, $symoffset, $snow_filestem) = @_;
     my $snow_net = $snow_filestem . '.net';
     my $snow_arch =     $snow_filestem . '.arch';
 #--- get unused port for SNoW
@@ -62,7 +135,7 @@ sub StartSNoW
 	# in child, start advisor
 	open STDOUT, '>', $snow_filestem . '.adv_out';
 	open STDERR, '>', $snow_filestem . '.adv_err';
-	exec("$path2advisor -p $sport -a $aport -o $snow_symoffset $snow_filestem")
+	exec("$path2advisor -p $sport -a $aport -o $symoffset $snow_filestem")
 	    or print STDERR "couldn't exec $path2advisor: $!";
 	exit(0);
     }
@@ -115,7 +188,8 @@ sub Tst1
 {
     my $BinDir = "/home/urban/gr/MPTP2/MizAR/cgi-bin/bin";
     my ($aport, $sport, $adv_pid, $snow_pid) = StartSNoW("$BinDir/snow", "$BinDir/advisor.pl", 500000, 'thms3');
-    sleep 120;
+    print "Advisor PID: $adv_pid, SNoW PID: $snow_pid\n";
+    sleep 110;
     my $input1 = ['k3_csspace3'];
     my $input2 = ['v2_rearran1'];
     my @refs1 = GetRefs('localhost', $aport, $input1, 10);
