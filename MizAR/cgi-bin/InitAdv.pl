a98c2252e1b129a06b87c5d2001eb628eb23de5d
Josef Urban <urban@eee.(none)>
2011-01-20 00:37:55 +0100

    more in InitAdv.pl

--- MizAR/cgi-bin/InitAdv.pl
+++ MizAR/cgi-bin/InitAdv.pl
@@ -8,13 +8,16 @@ use lib '/home/urban/gr/MPTP2/MizAR/cgi-bin';
 use AIAdvise;
 
 my $MizAR_path = '/home/urban/gr/MPTP2/MizAR/cgi-bin';
+my $snow = $MizAR_path . '/bin/snow';
+my $advisor = $MizAR_path . '/advisor_lean.pl';
 my $symoffset=500000;
 my $filestem= shift;
 my $advlimit= shift;
 my ($grefnr, $gsymnr, $gsymarity, $grefsyms, $gnrsym, $gnrref) = AIAdvise::CreateTables($symoffset, $filestem);
 my $proved_by = AIAdvise::PrintProvedBy0($symoffset, $filestem, $grefnr);
 AIAdvise::PrintTrainingFromHash($filestem,0,$proved_by,$grefnr, $gsymnr, $gsymarity, $grefsyms, $gnrsym, $gnrref);
-AIAdvise::Learn0( $MizAR_path . "/bin/snow", $filestem, $grefnr);
+
+AIAdvise::Learn0( $snow, $filestem, $grefnr);
 my ($aport, $sport, $adv_pid, $snow_pid) = 
-  AIAdvise::StartSNoW($MizAR_path . "/bin/snow", $MizAR_path . "/advisor_lean.pl", $symoffset, $filestem, $advlimit);
+  AIAdvise::StartSNoW($snow, $advisor", $symoffset, $filestem, $advlimit);
 print "$aport,$sport\n";
