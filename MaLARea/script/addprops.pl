7e07ca9dc4eefd826f7d1c5673c6ae526ed8536c
Josef Urban <Josef.Urban@gmail.com>
2011-10-26 21:22:02 +0200

    debug

--- MaLARea/script/addprops.pl
+++ MaLARea/script/addprops.pl
@@ -19,7 +19,7 @@ while(<F>)
     my @a = split(/[ ,]+/);
     $a[0] =~ m/^([a-z0-9_]+):theorem:(\d+)$/ or die "$_";
     $a[0] = 't' . $2 . '_' . $1;
-    $h{$a[0]} = ();
+    # $h{$a[0]} = (); this was stupid - only leavs last one
     $a[1] =~ m/^(Reflexivity|Commutativity|Symmetry|Connectedness|Irreflexivity|Abstractness|Projectivity|Idempotence|Antisymmetry|Involutiveness)$/ or die "$a[1]:$_";
     $a[2] =~ m/^([a-z0-9_]+):([gklmruv])constructor:(\d+)$/ or die "$_";
 
