16b3aa40ee2848114d208f31152f29cd9e26b79c
Josef Urban <Josef.Urban@gmail.com>
2012-01-20 11:35:44 +0100

    strictfied

--- MaLARea/script/snow2sofia.pl
+++ MaLARea/script/snow2sofia.pl
@@ -4,26 +4,29 @@
 #
 # perl -F snow2sofia.pl probs6.train_01 > probs6.softrain
 
+use strict;
 
-$z=10000;
+my %h=();    # hash keeping contiguous numbering for pars of features
+my $z=10000; # max bound for premise numbering - we start the pair numbering here
+
 while (<>)
 {
-    @l= m/(\d+)/g;
-    $q=$l[0];
-    @t=grep {$_ < 2000000} @l;
-    @f=grep {$_ >= 2000000} @l;
-    $r=2;
-    foreach $j (@t)
+    my @l= m/(\d+)/g;
+    my $q=$l[0];
+    my @t=grep {$_ < 2000000} @l;
+    my @f=grep {$_ >= 2000000} @l;
+    my $r=2;  # bigger weight for the name of the formula than for the premises
+    foreach my $j (@t)
     {
 	print "$r qid:$q ";
 	$r=1;
-	@f1=();
-	foreach $i (@f)
+	my @f1=();
+	foreach my $i (@f)
 	{
 	    if (!exists($h{"$j:$i"})) {  $h{"$j:$i"}=$z++;	    }
 	    push(@f1,$h{"$j:$i"})
 	}
-	foreach $k (sort {$a<=>$b} @f1) { print " $k:1";	}
+	foreach my $k (sort {$a<=>$b} @f1) { print " $k:1";	}
 	print "\n";
     }
 }
