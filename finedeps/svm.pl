9f99ada20f1dbb4be3c29af6fa007f20edacd70e
Josef Urban <Josef.Urban@gmail.com>
2012-04-18 16:38:41 +0200

    Produce the svm format from ours

--- finedeps/svm.pl
+++ finedeps/svm.pl
@@ -25,3 +25,11 @@ while(<>)
     foreach $r (@r) {print $r," ",$f1,":1\n" }
 }
 
+# select conjectures
+# perl -e 'open(F,"00chordth1") or die; @t=<F>; @ht{@t}=(); open(G,"probs6.refnr") or die; @r=<G>; foreach $rr (@r) {$hr{$rr}=$i; $i++;  } while(<>) {m/^(\d+) .*/ or die; if(exists $ht{$r[$1]}) { print $_  }   }'  svmdata.tst2220 > svmdata.tst2078
+
+# predict
+# time ./svm_multiclass_classify exmptp/svmdata.tst2078 exmptp/model1all exmptp/predt2078 | tee 00allpred2078log 
+
+# create predictions
+# time perl -e 'while(<>) { chop; m/^\d+ (.*)/; @r=split(/ +/,$1); %h=(); foreach $i (0..$#r) {$h{$i+1}=$r[$i];  } @s=sort {$h{$b} <=> $h{$a}} (keys %h); print join(",",@s[0..500]), "\n"  }' predt2220 >predt2220.srt2
