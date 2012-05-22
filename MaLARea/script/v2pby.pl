7325e347ada841776eebb489b511974a38871778
Josef Urban <Josef.Urban@gmail.com>
2012-05-22 16:52:47 +0200

    comment

--- MaLARea/script/v2pby.pl
+++ MaLARea/script/v2pby.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 
 # ./v2pby conjname foo.vout > foo.pby
+# for i in `ls | sed -e 's/.p.vout$//'`; do /home/mptp/gr/MPTP2/MaLARea/script/v2pby.pl $i $i.p.vout; done > 00proved_by
 
 my $vampire_regexp = '.*\bfile\([^\),]+, *([a-z0-9A-Z_]+) *\)';
 
