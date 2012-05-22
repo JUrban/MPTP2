86d5f24f945dd5224238143020a0be7b0b90864c
Josef Urban <Josef.Urban@gmail.com>
2012-05-22 17:31:25 +0200

    comment

--- MaLARea/script/v2pby.pl
+++ MaLARea/script/v2pby.pl
@@ -2,6 +2,9 @@
 
 # ./v2pby conjname foo.vout > foo.pby
 # for i in `ls | sed -e 's/.p.vout$//'`; do /home/mptp/gr/MPTP2/MaLARea/script/v2pby.pl $i $i.p.vout; done > 00proved_by
+# 
+# the same thing for z3 is simpler:
+# cat *|grep '^core'  | sed -e 's/^core(\([^,]*\),\[/proved_by(\1,[\1,/' > 00zproved_by
 
 my $vampire_regexp = '.*\bfile\([^\),]+, *([a-z0-9A-Z_]+) *\)';
 
