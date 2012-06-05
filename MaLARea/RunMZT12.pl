447dcc350c06c1a2a33de012564a3b749e60616c
Josef Urban <Josef.Urban@gmail.com>
2012-06-05 06:10:02 +0200

    final params

--- MaLARea/RunMZT12.pl
+++ MaLARea/RunMZT12.pl
@@ -5,7 +5,7 @@
 
 ## SYNOPSIS
 
-## ./RunMZT12.pl /home/mptp/gr/MPTP2/MaLARea /home/mptp/gr/MPTP2/MaLARea/doc/BatchTstMZRMZR
+## ./RunMZT12.pl /home/mptp/gr/MPTP2/MaLARea /home/mptp/gr/MPTP2/MaLARea/doc/BatchTst100MZRMZR
 
 use strict;
 
@@ -29,4 +29,6 @@ chdir($tmpdir);
 
 local $SIG{'XCPU'} = sub { `rm -r -f /dev/shm/$prefix`; die "Killed by SIXXCPU"; };
 
-exec("time ./TheoryLearner.pl  -o\"$ltbinput\" --dummy=\"data/dummyallaxmzt\" -F1 -z1 -e\"$prefix/\" -T\"/dev/shm/\" -C4 -A256   --loadprovedby=\"data/MZT1000TrainingData\" -f2 -O1 -p128 -y1  -u1   -t400 -S0  -P1 -D1 -l1 -M0 -b1 -w6 -i5 -L15000 $prefix  |tee $prefix.log");
+# exec("time ./TheoryLearner.pl  -o\"$ltbinput\" --dummy=\"data/dummyallaxmzt\" -F1 -z1 -e\"$prefix/\" -T\"/dev/shm/\" -C4 -A256   --loadprovedby=\"data/MZT1000TrainingData\" -f2 -O1 -p128 -y1  -u1   -t400 -S0  -P1 -D1 -l1 -M0 -b1 -w6 -i5 -L15000 $prefix  |tee $prefix.log");
+
+exec("time ./TheoryLearner.pl  -o\"$ltbinput\" --runepar=1 --dummy=\"data/dummyallaxmzt\" -F1 -z1 -e\"$prefix/\" -T\"/dev/shm/\" -C1 -A512   --loadprovedby=\"data/MZT1000TrainingData\" -f2 -O0 -p64 -y1  -u0   -t400 -S0  -P1 -D1 -l1 -M0 -b1 -w6 -i5 -L15000 $prefix  |tee $prefix.log");
